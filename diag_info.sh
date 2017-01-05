947dead612b8124690caff3a79ef2ed4819a6419
Dave Robinson <robboz4@gmail.com>
2017-01-04 17:35:27 -0800

    Anomaly Fixing
    Tidied up some code and fixed a Pi 3 issue with iwlist wlan0 scan .
    Added a 5 second sleep and it works. Still looking into an issue where
    not all the file gets uploaded to pastern. Also added df and got a
    better format for gpis output. tested on P3 and P2. Exceed pastebin
    uploads for 24 hour period so will test more tomorrow. Might add more
    Pi related command for memory and temperature.

--- diag_info.sh
+++ diag_info.sh
@@ -22,17 +22,25 @@
 # Altered name for upload to pastebin.com
 # Bumped   the version  number to 2.0.2
 # Bumped   the version number to 2.0.3 Added PiGMi data collection
+# Bumped   the version number to 2.0.4 Added df -h, version string, hostname
+# Rearranged the collection of data. Added a parameter to dmesg to keep the output
+# smaller. Not all the log gets posted to pastebin, but all data is captured.
+#  
 
 # Notes:
 # Still working on formatting better for xml output. The links to Tom's file should work.
 # 
-# 
+# pastebin not getting a complete upload...
 
 
 TODAY=`date +%F%M`
 #Used to generate a sudo unique file name
 Log="Diagout$TODAY.txt"
 PASTEBIN_Name="Diagout$TODAY."
+echo "This program can create a large file that might not get fully uploaded to pastebin."
+echo "Please keep the file in case the requester needs information that did not get to pastebin."
+echo "Starting to collect data to file " $Log
+
 #Creates file with xml head
 echo "<?xml version=\"1.0\" ?>" >$Log
 echo "<?xml-stylesheet type='text/xsl' href='http://tommilner.org/diagout/diagout.xsl' ?>" >> $Log
@@ -41,11 +49,14 @@ echo "<?xml-stylesheet type='text/xsl' href='http://tommilner.org/diagout/diagou
 #Opening tag
 echo "<log>" >>$Log
 echo "Log created on " `date` >>$Log
+echo "diag_info version = 2.0.4 " >> $Log
 
 #System name
 echo "<system>">> $Log
 echo " ON SYSTEM: " >>$Log
 uname -a  >> $Log
+echo "Known as:" >> $Log
+hostname  >> $Log
 
 echo "Octoprint Version:"  `cat /etc/octopi_version`  >> $Log
 
@@ -57,10 +68,12 @@ Php_ver=$(php -v 2>&1)
 echo "PHP Version: " $Php_ver  >> $Log
 GPIO_ver=$(gpio -v 2>&1 | head -n1)
 echo "gpioutility Version: "  $GPIO_ver >> $Log
-GPIO_Set=$(gpio readall 2<&1)
+
 echo "GPIO Pin settings: "  >> $Log 
 echo ""  >> $Log
-echo $GPIO_Set >> $Log
+gpio readall >> $Log
+echo "Disk Space:" >> $Log
+sudo df -h >> $Log 
 echo "</system>">> $Log
 
 #lsusb data
@@ -75,20 +88,6 @@ echo "  LSMOD OUPUT:" >> $Log
 sudo lsmod >>  $Log
 echo "</lsmod>" >> $Log
 
-#dmesg data
-echo "<dmesg>" >> $Log
-echo " DMESG OUTPUT:  " >>$Log
-dmesg >> $Log
-echo "</dmesg>" >> $Log
-
-#/var/log/syslog last 100 lines only
-
-echo "<SysLog>">> $Log
-#SysLog=`tail -100 /var/log/syslog` 
-tail -100 /var/log/syslog >> $Log
-#echo $SysLog >> $Log
-echo "</SysLog>" >> $Log
-
 #wifi data
 echo "<wifi>" >>$Log
 echo  " WIFI OUTPUT: " >>$Log
@@ -99,9 +98,29 @@ echo "<wpa_cli>" >> $Log
 sudo wpa_cli scan >> $Log && sudo wpa_cli scan_results >> $Log
 echo "</wpa_cli>" >>$Log
 echo "<iwlist>" >>$Log
+sleep 5
+# Above sleep added for P3.
 sudo iwlist wlan0 scan >>$Log
 echo "</iwlist>" >>$Log
 echo "</wifi>" >>$Log
+
+echo "<SysLog>">> $Log
+#SysLog=`tail -100 /var/log/syslog` 
+tail -100 /var/log/syslog >> $Log
+#echo $SysLog >> $Log
+echo "</SysLog>" >> $Log
+
+
+#dmesg data
+echo "<dmesg>" >> $Log
+echo " DMESG OUTPUT:  " >>$Log
+dmesg -l info >> $Log
+echo "</dmesg>" >> $Log
+
+#/var/log/syslog last 100 lines only
+
+
+
 
 echo " End of data captured. " >>$Log
 
@@ -140,12 +159,18 @@ then
     Paste_url=$(curl -d "${querystring}" http://pastebin.com/api/api_post.php )
 
     echo "Please pass this url to the requester of the information." $Paste_url
+    echo " The whole file may not get transmitted to pastebin. So please save it in case the requester needs more information"
+
 
 
 else
   	echo "No Network must paste file manually " 
   	echo ""
   	echo "Please cut & paste the file " $Log " to www.pastebin.com and pass the link to the requester."
+  	echo "The whole file may not get pasted  to pastebin. So please save it in case the requester needs more information"
+
+
+  	
 fi
 
 # The End.
